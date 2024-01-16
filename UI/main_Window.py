93b7bcd9e5d21e568fa68bef552b0ce152eec6db
awaisAhmed19 <awaisahmedoffi@gmail.com>
2024-01-16 22:25:16 +0530

    making changes in gui and implementing the calc engine in the UI

--- UI/main_Window.py
+++ UI/main_Window.py
@@ -1,38 +1,46 @@
 import PySimpleGUI as sg
+import math
+import sympy as sp
+from calc_Engine import *
 
-#from calc_Engine.ExpressionChecker import *
-#from calc_Engine.Postfix_Evaluation import Post_Evaluation
 
-#PE=Post_Evaluation()
+pr=Precedence()
+tk=Tokenization()
+Pe=Postfix_Evaluation()
+
+
+Unicode_Dic={
+    'logx':"log\u2090x",
+    'div':"\u00F7",
+    'mul':"\u00D7"
+}
 
 layout = [
-    [sg.Multiline(size=(50,7),key="-Display-",no_scrollbar=True)],
-    [sg.Button("log\u2090x",size=(5,2)),sg.Button("x\u00B2",size=(5,2)),sg.Button("a/b",size=(5,2)),sg.Button('\u221A',size=(5,2)),sg.Button("x\u00B2",size=(5,2))],
-    [sg.Button("x\u207F",size=(5,2)),sg.Button("log",size=(5,2)),sg.Button("ln",size=(5,2)),sg.Button("hyp",size=(5,2)),sg.Button("x\u00B3",size=(5,2))],
-    [sg.Button("Sin",size=(5,2)),sg.Button("Tan",size=(5,2)),sg.Button("Cos",size=(5,2)),sg.Button("e\u207F",size=(5,2)),sg.Button("x\u221Ax",size=(5,2))],
-    [sg.Button("7",size=(5,2)),sg.Button("8",size=(5,2)),sg.Button("9",size=(5,2)),sg.Button("C",size=(5,2)),sg.Button("AC",size=(5,2))],
-    [sg.Button("4",size=(5,2)),sg.Button("5",size=(5,2)),sg.Button("6",size=(5,2)),sg.Button(u'\u00F7',size=(5,2)),sg.Button("\u00D7",size=(5,2))],
-    [sg.Button("2",size=(5,2)),sg.Button("2",size=(5,2)),sg.Button("3",size=(5,2)),sg.Button("+",size=(5,2)),sg.Button("-",size=(5,2))],
-    [sg.Button("0",size=(5,2)),sg.Button(".",size=(5,2)),sg.Button("=",size=(5,2)),sg.Button("Ans",size=(5,2)),sg.Button("Exp",size=(5,2))]
-    
+    [sg.Multiline(size=(50, 7), key="-Display-", no_scrollbar=True)],
+    [sg.Button("7", size=(5, 2)), sg.Button("8", size=(5, 2)), sg.Button("9", size=(5, 2)),sg.Button("C", size=(5, 2)), sg.Button("AC", size=(5, 2))],
+    [sg.Button("4", size=(5, 2)), sg.Button("5", size=(5, 2)), sg.Button("6", size=(5, 2)),sg.Button(Unicode_Dic["div"], size=(5, 2)), sg.Button(Unicode_Dic["mul"], size=(5, 2))],
+    [sg.Button("1", size=(5, 2)), sg.Button("2", size=(5, 2)), sg.Button("3", size=(5, 2)),sg.Button("+", size=(5, 2)), sg.Button("-", size=(5, 2))],
+    [sg.Button("0", size=(5, 2)), sg.Button(".", size=(5, 2)), sg.Button("=", size=(5, 2)),sg.Button("Ans", size=(5, 2)), sg.Button("Exp", size=(5, 2))]
 ]
 
-window = sg.Window("Scientific Calculator", layout,size=(310,500),resizable=True,finalize=True)
+expression=""
+window = sg.Window("Scientific Calculator", layout, size=(310, 500), resizable=True, finalize=True)
 
 while True:
     event, values = window.read()
 
     if event == sg.WINDOW_CLOSED:
         break
-    elif event in "0123456789.+-x u'\u00F7'":
-        window["-Display-"].update(values["-Display-"]+event)
-    elif event=="=":
-        try:                
-            result=eval(values["-Display-"])
-            window["-Display-"].update(str(result))
-        except:
-            window["-Display-"].update("Error")
-    elif event=="C":
-        window["-Display-"].update("")
+    elif event.isdigit():
+        expression +=event
+        window["-Display-"].update(expression)
+    elif event in pr.OPERATORS:
+        expression+=event
+        window["-Display-"].Update(expression)
+    elif event =='=':
+        print(expression)
+
+
     
+
 window.close()
---
+++ calc_Engine/__init__.py
@@ -0,0 +1,1 @@
+__all__=["ExpressionChecker","calculations","Postfix_Evaluation","PostfixConv","Precedence","Tokenization","TrignometricTable"]
