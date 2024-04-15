3ec94fc6815f7bc4525851e59d2a67bb18bc19f6
Awais Ahmed <awaisahmedoffi@gmail.com>
2024-04-16 01:28:08 +0530

    made some more buttons and got the layout setup

--- calc_Engine/Gui.py
+++ calc_Engine/Gui.py
@@ -3,13 +3,23 @@ import pygame as pg
 import os 
 pg.init()
 
-WIDTH,HEIGHT=460,600
+WIDTH,HEIGHT=68*5,600
 screen=pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption("Scientific Calculator")
 clock=pg.time.Clock()
 parent_dir=os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir))
 normalButtons=os.path.join(parent_dir,"normalButtons")
 pressedButtons=os.path.join(parent_dir,"pressedButtons")
+
+button_types=[
+        "sin","cos","tan","rad","deg",
+        "log","ln","(",")","inv",
+        "!","AC","%","back","div",
+        "^","7","8","9","mul",
+        "sqrt","4","5","6","sub",
+        "pie","1","2","3","add",
+        "e","00","0","dot","equal", 
+]
 
 normalButtons_img_path=[]
 for file in os.listdir(normalButtons):
@@ -19,6 +29,9 @@ pressedButtons_img_path=[]
 for file in os.listdir(pressedButtons):
     if os.path.isfile(os.path.join(pressedButtons, file)):
         pressedButtons_img_path.append(file)
+
+normalButtons_img_path=[f"{i}.png" for i in button_types]
+pressedButtons_img_path=[f"{i}.png" for i in button_types]
 
 normalButton_img=[]
 pressedButton_img=[]
@@ -31,7 +44,6 @@ for button in pressedButtons_img_path:
     img_path=os.path.join(pressedButtons,button)
     buttons=pg.image.load(img_path).convert_alpha()
     pressedButton_img.append(buttons)
-
 
 class Button:
     def __init__(self,x,y,normal_img,pressed_img):
@@ -56,26 +68,27 @@ class Button:
 
 def main():
 
-    NUM_ROWS=3
-    NUM_COLS=3
+    NUM_ROWS=7
+    NUM_COLS=5
+
 
     BUTTON_WIDTH=64
     BUTTON_HEIGHT=64
     
-    BUTTON_PADDING_X=10
-    BUTTON_PADDING_Y=10
+    BUTTON_PADDING_X=2
+    BUTTON_PADDING_Y=0
     
     GRID_WIDTH=NUM_COLS*(BUTTON_WIDTH+BUTTON_PADDING_X)
     GRID_HEIGHT=NUM_ROWS*(BUTTON_HEIGHT+BUTTON_PADDING_Y)
 
     GRID_START_X=(WIDTH-GRID_WIDTH)//2
-    GRID_START_Y=(HEIGHT-GRID_HEIGHT)//2
+    GRID_START_Y=(HEIGHT-GRID_HEIGHT)
     buttons=[]
     for row in range(NUM_ROWS):
         for col in range(NUM_COLS):
+            index=row*NUM_COLS+col
             x = GRID_START_X + col * (BUTTON_WIDTH + BUTTON_PADDING_X)
             y = GRID_START_Y + row * (BUTTON_HEIGHT + BUTTON_PADDING_Y)
-            index = row * NUM_COLS + col  # Calculate the index of the button image
             button = Button(x, y, normalButton_img[index], pressedButton_img[index])
             buttons.append(button)
 
@@ -91,7 +104,7 @@ def main():
         for button in buttons:
             button.update(mouse_pos,mouse_pressed)
 
-        screen.fill((255,255,255))
+        screen.fill((182,182,182))
         for button in buttons:
             button.draw(screen)
         pg.display.flip()
--- calc_Engine/buff.py
+++ calc_Engine/buff.py
@@ -5,6 +5,17 @@ WIDTH,HEIGHT=460,600
 screen=pg.display.set_mode((WIDTH,HEIGHT))
 pg.display.set_caption("Scientific Calculator")
 clock=pg.time.Clock()
+
+button_types=[
+        "sin","cos","tan","rad","deg",
+        "log","ln","(",")","inv",
+        "!","AC","%","back","div",
+        "^","7","8","9","mul",
+        "sqrt","4","5","6","sub",
+        "pie","1","2","3","add",
+        "e","00","0","dot","equal",
+        
+    ]
 parent_dir=os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir))
 normalButtons=os.path.join(parent_dir,"normalButtons")
 pressedButtons=os.path.join(parent_dir,"pressedButtons")
@@ -17,13 +28,16 @@ pressedButtons_img_path=[]
 for file in os.listdir(pressedButtons):
     if os.path.isfile(os.path.join(pressedButtons, file)):
         pressedButtons_img_path.append(file)
-
+normalButtons_img_path=[f"{normalButtons}/{i}.png" for i in button_types]
 normalButton_img=[]
 pressedButton_img=[]
 for button in normalButtons_img_path:
     img_path=os.path.join(normalButtons,button)
     buttons=pg.image.load(img_path).convert_alpha()
     normalButton_img.append(buttons)
+
+print(normalButton_img)
+print(normalButtons_img_path)
 
 for button in pressedButtons_img_path:
     img_path=os.path.join(pressedButtons,button)
